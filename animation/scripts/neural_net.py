67596a5cd5ef6e953f90a1297469c616d2c7f3a7
Cason Wight <cason.wight@gmail.com>
2023-01-28 19:35:12 -0700

    finished transformer and other animations

--- animation/scripts/neural_net.py
+++ animation/scripts/neural_net.py
@@ -52,12 +52,12 @@ class FunctionEstimator(Scene):
         return func
 
     def construct(self):
+        """ The audience doesn't know what a neural network is, so we need to start from the beginning.
+        We want to show that a neural network is a universal function approximator, and that it can be used
+        to approximate any function. We want to show this by showing that it can approximate a function that
+        takes in a picture, a chess board, and a text prompt, and outputs a response.
+        """
         self.camera.background_color = WHITE
-
-        func_wrap = VGroup(
-            Tex("$f($", color=BLACK),
-            Tex("$)=$", color=BLACK)
-        ).arrange(RIGHT, buff=1).move_to(ORIGIN)
 
         # Pictures
         cat1 = ImageMobject(ASSET_PATH + "cat-1.jpg").scale_to_fit_width(5)
@@ -67,6 +67,10 @@ class FunctionEstimator(Scene):
         labs_vals = [r"\text{cat}", r"\text{dog}", r"\text{cat}"]
         labels = VGroup(*[Tex(lab, color=RED) for lab in labs_vals])
 
+        func_wrap = VGroup(
+            Tex("$f($", color=BLACK),
+            Tex("$)=$", color=BLACK)
+        ).arrange(RIGHT, buff=5 + 1).move_to(ORIGIN).shift(labels[1].get_width()/2 * LEFT)
         func_wrap = self.ticker_anim(animals, labels, func_wrap, pause_time=3/4)
 
         # Chess
@@ -92,5 +96,93 @@ class FunctionEstimator(Scene):
 
         func_wrap = self.ticker_anim(prompts, responses, func_wrap, pause_time=1)
         
-        func_wrap2 = func_wrap.copy().arrange(RIGHT, buff=1).move_to(ORIGIN)
+        func_wrap2 = func_wrap.copy().arrange(RIGHT, buff=5 + 1).move_to(ORIGIN).shift(labels[1].get_width()/2 * LEFT)
         self.play(Transform(func_wrap, func_wrap2))
+
+class SimplifiedNeuralNetwork(Scene):
+    def construct(self):
+        """ After showing that neural networks are universal function approximators, 
+        we show how a simple neural network is structured.
+        We also want to show that as a network sees example input-output pairs, it 
+        makes adjustments to its weights and biases to better approximate the function.
+        
+        We want to use word prediction as the example, because we will move to transformers
+        Everything should be pretty simple, because this is a non-technical audience.
+        """
+        self.camera.background_color = WHITE
+
+        # Show a network with 5 inputs, 2 hidden layers, and 5 output
+        inputs = VGroup(*[Circle(color=BLACK).set_fill(BLACK, opacity=.5) for _ in range(3)]).arrange(DOWN)
+        hidden1 = VGroup(*[Circle(color=BLUE).set_fill(BLUE, opacity=.5) for _ in range(5)]).arrange(DOWN)
+        hidden2 = VGroup(*[Circle(color=BLUE).set_fill(BLUE, opacity=.5) for _ in range(5)]).arrange(DOWN)
+        outputs = VGroup(*[Circle(color=RED).set_fill(RED, opacity=.5) for _ in range(3)]).arrange(DOWN)
+        neurons = VGroup(inputs, hidden1, hidden2, outputs).arrange(RIGHT, buff=2)
+        input_label = Tex("Inputs", color=BLACK).next_to(inputs, UP)
+        output_label = Tex("Outputs", color=BLACK).next_to(outputs, UP)
+        neurons.add(input_label, output_label)
+
+        input_hidden1_lines = VGroup(*[Line(input.get_right(), output.get_left(), color=GREY_A) for input in inputs for output in hidden1])
+        hidden1_hidden2_lines = VGroup(*[Line(input.get_right(), output.get_left(), color=GREY_A) for input in hidden1 for output in hidden2])
+        hidden2_output_lines = VGroup(*[Line(input.get_right(), output.get_left(), color=GREY_A) for input in hidden2 for output in outputs])
+        connections = VGroup(input_hidden1_lines, hidden1_hidden2_lines, hidden2_output_lines)
+        net = VGroup(
+            inputs, input_hidden1_lines, 
+            hidden1, hidden1_hidden2_lines, 
+            hidden2, hidden2_output_lines, 
+            outputs, 
+            input_label, output_label
+        ).scale(1/2).move_to(ORIGIN)
+
+        self.add(net)
+        self.wait(2)
+
+        input_text = Tex('``To be or not "', color=BLACK).scale(.75).next_to(net, LEFT, buff=.5)
+        tokenized_input_text = Tex('(042, 832, 194, 384)', color=BLACK).scale(.65).next_to(net, LEFT, buff=.5)
+        output_text = Tex('``to be"', color=RED).scale(.75).next_to(net, RIGHT, buff=.5)
+        tokenized_output_text = Tex('(042, 832)', color=RED).scale(.65).next_to(net, RIGHT, buff=.5)
+
+        self.play(FadeIn(input_text, shift=RIGHT))
+        self.wait(2)
+
+        self.play(Transform(input_text, tokenized_input_text))
+        self.wait(2)
+
+        self.play(ReplacementTransform(input_text, inputs))
+        self.wait(2)
+
+        self.play(*(input.animate.set_fill(color=BLACK, opacity=.75) for input in inputs), run_time=.5)
+        self.play(
+            *(input.animate.set_fill(color=BLACK, opacity=.5) for input in inputs),
+            *(line.animate.set_color(GREY_B) for line in input_hidden1_lines), run_time=.5
+        )
+        self.play(
+            *(hidden.animate.set_fill(color=BLUE, opacity=.75) for hidden in hidden1),
+            *(line.animate.set_color(GREY_A) for line in input_hidden1_lines), run_time=.5
+        )
+        self.play(
+            *(hidden.animate.set_fill(color=BLUE, opacity=.5) for hidden in hidden1),
+            *(line.animate.set_color(GREY_B) for line in hidden1_hidden2_lines), run_time=.5
+        )
+        self.play(
+            *(hidden.animate.set_fill(color=BLUE, opacity=.75) for hidden in hidden2),
+            *(line.animate.set_color(GREY_A) for line in hidden1_hidden2_lines), run_time=.5
+        )
+        self.play(
+            *(hidden.animate.set_fill(color=BLUE, opacity=.5) for hidden in hidden2),
+            *(line.animate.set_color(GREY_B) for line in hidden2_output_lines), run_time=.5
+        )
+        self.play(
+            *(output.animate.set_fill(color=RED, opacity=.75) for output in outputs),
+            *(line.animate.set_color(GREY_A) for line in hidden2_output_lines), run_time=.5
+        )
+        self.play(*(output.animate.set_fill(color=RED, opacity=.5) for output in outputs), run_time=.5)
+        self.wait(2)
+        self.play(TransformFromCopy(outputs, tokenized_output_text))
+        self.wait(2)
+        self.play(Transform(tokenized_output_text, output_text))
+        self.wait(2)
+
+        self.play(FadeOut(tokenized_output_text))
+        
+
+
--- animation/scripts/transformer.py
+++ animation/scripts/transformer.py
@@ -1,5 +1,153 @@
 from manim import *
 
+
+ASSET_PATH = "animation/assets/"
+
 class Transformer(Scene):
     def construct(self):
-        pass
+        self.camera.background_color = WHITE
+
+        # Words to ingest are in the middle of the screen
+        text = r"$\textlangle\text{SOS}\textrangle$ To be or not".split()
+        words = VGroup(*[Tex(word, color=GREY_A) for word in text]).arrange(RIGHT, buff=3/4).move_to(ORIGIN).shift(UP + LEFT * 2)
+        for word in words[1:]:
+            word.align_to(words[0], DOWN)
+
+        # Encoder will appear below the first word
+        encoder = RoundedRectangle(height=1, width=2, corner_radius=0.25, color=BLUE).set_fill(opacity=0.5)  
+        encoder.scale(3/4).next_to(words[0], DOWN)
+
+        # Encoder has gears in it
+        gear = SVGMobject(ASSET_PATH + "gear.svg").set_fill(color=GREY, opacity=1)
+        encoder_gears = VGroup(
+            gear.copy().shift(LEFT+UP),
+            gear.copy().shift(RIGHT),
+            gear.copy().scale(3/4).shift(LEFT * 3/4 + DOWN),
+        ).scale(1/8).move_to(encoder.get_center())
+
+        # Decoder is below the encoder
+        decoder = RoundedRectangle(height=2, width=4, corner_radius=0.25, color=PURPLE).set_fill(opacity=0.5).scale(3/4).next_to(encoder, DOWN).align_to(encoder, LEFT)
+
+        # Decoder has gears
+        decoder_gears = encoder_gears.copy().align_to(decoder, UR).shift(LEFT * 1/8 + DOWN * 1/8)
+        
+        # Decoder will have words
+        decoded_text = r"To be or not to be".split()
+        decoded_words = VGroup(*[Tex(word, color=BLACK) for word in decoded_text]).scale(1/2).arrange(RIGHT, buff=1/8).set_opacity(0)
+        for word in decoded_words[1:]:
+            word.align_to(decoded_words[0], DOWN)
+        decoded_words.move_to(decoder.get_center())
+        decoded_words.add(Tex(r"$\textlangle\text{EOS}\textrangle$").set_opacity(0))
+        
+        # Decoder will have label
+        decoder_label = Tex("Decoder", color=BLACK).scale(1/2).next_to(decoder, LEFT)
+
+        # Encoder will have label
+        encoder_label = Tex("Encoder", color=BLACK).scale(1/2).add_updater(lambda x: x.next_to(encoder, LEFT))
+
+        # The whole thing moves together as a transformer
+        transformer = VGroup(
+            encoder,
+            encoder_gears,
+            decoder,
+            decoder_gears,
+            encoder_label,
+            decoder_label,
+            decoded_words
+        )
+        
+        self.play(Write(words))
+        self.wait()
+
+        self.play(
+            FadeIn(transformer, shift=RIGHT),
+            words[0].animate.set_color(BLACK)
+        )
+        self.wait()
+        shift_dist = (words[1].get_center() - encoder.get_center())[0]
+
+        self.play(
+            *(Rotate(gear, angle=(int(i % 2 == 1) * 2 - 1) * PI, about_point=gear.get_center(), rate_func=linear) for i, gear in enumerate(encoder_gears)),
+            words[0].animate.move_to(encoder_gears.get_center()).scale(0),
+        )  
+        self.remove(words[0])
+        self.play(*(Rotate(gear, angle=(int(i % 2 == 1) * 2 - 1) * PI, about_point=gear.get_center(), rate_func=linear) for i, gear in enumerate(encoder_gears)))
+
+        for word, next_word, decoded_word in zip(words[1:], list(words[2:]) + [words[-1]], decoded_words[:-2]):
+            self.play(
+                transformer.animate.shift(RIGHT * shift_dist),
+                word.animate.set_color(BLACK),
+            )
+            self.wait()
+            
+            shift_dist = (next_word.get_center() - encoder.get_center())[0]
+            
+            self.play(
+                *(Rotate(gear, angle=(int(i % 2 == 1) * 2 - 1) * PI, about_point=gear.get_center(), rate_func=linear) for i, gear in enumerate(encoder_gears)),
+                word.animate.move_to(encoder_gears.get_center()).scale(.01),
+            )
+            self.play(
+                *(Rotate(gear, angle=(int(i % 2 == 1) * 2 - 1) * PI, about_point=gear.get_center(), rate_func=linear) for i, gear in enumerate(encoder_gears)),
+                *(Rotate(gear, angle=(int(i % 2 == 1) * 2 - 1) * PI, about_point=gear.get_center(), rate_func=linear) for i, gear in enumerate(decoder_gears)),
+                ReplacementTransform(word, decoded_word),
+                decoded_word.animate.set_opacity(1),
+            )
+            self.remove(word)
+            self.wait()
+
+        
+        pred_words = [
+            ["today (5\%)", "to (93\%)", r"$\textlangle\text{EOS}\textrangle$ (2\%)"],
+            ["be (94\%)", "think (3\%)", r"$\textlangle\text{EOS}\textrangle$ (3\%)"],
+            ["not (1\%)", ". (40\%)", r"$\textlangle\text{EOS}\textrangle$ (59\%)"]
+        ]
+
+        correct_words = [1, 0, 2]
+        incorrect_words = [
+            [0, 2],
+            [1, 2],
+            [0, 1]
+        ]
+
+        for i, (correct, incorrect, poss_words, decoded_word) in enumerate(zip(correct_words, incorrect_words, pred_words, decoded_words[-3:])):
+            correct_word = Tex(poss_words[correct], color=BLACK).scale(1/2)
+            incorrect_word_1 = Tex(poss_words[incorrect[0]], color=BLACK).scale(1/2)
+            incorrect_word_2 = Tex(poss_words[incorrect[1]], color=BLACK).scale(1/2)
+            pred_words = VGroup(incorrect_word_1, correct_word, incorrect_word_2).arrange(DOWN).next_to(decoder, RIGHT)
+
+            incorrect_word_1.align_to(correct_word, LEFT)
+            incorrect_word_2.align_to(correct_word, LEFT)
+
+            self.play(
+                *(Rotate(gear, angle=(int(i % 2 == 1) * 2 - 1) * PI, about_point=gear.get_center(), rate_func=linear) for i, gear in enumerate(decoder_gears)),
+                TransformFromCopy(decoded_words, pred_words)
+            )
+            self.wait()
+
+            self.play(
+                FadeOut(incorrect_word_1),
+                FadeOut(incorrect_word_2),
+            )
+
+            self.play(
+                correct_word.animate.move_to(encoder_gears.get_center()).scale(.05),
+                *(Rotate(gear, angle=(int(i % 2 == 1) * 2 - 1) * PI, about_point=gear.get_center(), rate_func=linear) for i, gear in enumerate(encoder_gears)),
+                
+            )
+            if i < 2:
+                self.play(
+                    *(Rotate(gear, angle=(int(i % 2 == 1) * 2 - 1) * PI, about_point=gear.get_center(), rate_func=linear) for i, gear in enumerate(encoder_gears)),
+                    *(Rotate(gear, angle=(int(i % 2 == 1) * 2 - 1) * PI, about_point=gear.get_center(), rate_func=linear) for i, gear in enumerate(decoder_gears)),
+                    Transform(correct_word, decoded_word),
+                    decoded_word.animate.set_opacity(1),
+                )
+            else:
+                self.play(
+                    *(Rotate(gear, angle=(int(i % 2 == 1) * 2 - 1) * PI, about_point=gear.get_center(), rate_func=linear) for i, gear in enumerate(encoder_gears)),
+                    FadeOut(correct_word)
+                )
+    
+
+        self.play(
+            FadeOut(transformer, shift=RIGHT)
+        )
